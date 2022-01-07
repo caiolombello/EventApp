6b307f5f5fe5a8ebad19337cdc17218ce13b2b2c
caiol <caiolvbarbieri@pm.me>
2022-01-07 19:20:26 -0300

    Database (create & config)

--- src/main/java/com/eventsapp/controllers/IndexController.java
+++ src/main/java/com/eventsapp/controllers/IndexController.java
@@ -1,4 +1,4 @@
-package com.eventapp.eventapp;
+package com.eventapp.eventapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
